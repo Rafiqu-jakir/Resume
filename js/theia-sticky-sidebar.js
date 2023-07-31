04d997b70e8d9b2b86a075d1ed1729b472a5859b
Rafiqul Islam <rafiquljakir@gmail.com>
2023-08-01 00:55:04 +0600

    Update theia-sticky-sidebar.js

--- js/theia-sticky-sidebar.js
+++ js/theia-sticky-sidebar.js
@@ -1,12 +1,4 @@
-/*!
- * Theia Sticky Sidebar v1.5.0
- * https://github.com/WeCodePixels/theia-sticky-sidebar
- *
- * Glues your website's sidebars, making them permanently visible while scrolling.
- *
- * Copyright 2013-2016 WeCodePixels and other contributors
- * Released under the MIT license
- */
+
 
 (function ($) {
     $.fn.theiaStickySidebar = function (options) {
